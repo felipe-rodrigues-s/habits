05ba6df53fc772f1877051426c65f7bff7146787
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2023-01-17 21:26:56 -0300

    get day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,4 +27,30 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+    const parseDate = dayjs(date).startOf("day");
+    const weekDay = parseDate.get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+    return {
+      possibleHabits,
+    };
+  });
 }
