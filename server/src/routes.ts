a7c210a63bb7c3aedec27718be0e27f4b65dd009
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2023-01-17 21:06:13 -0300

    zod

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,28 @@
 import { prisma } from "./lib/prisma";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/habits", async (request) => {
-    const { title, weekDays } = request.body;
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
   });
 }
