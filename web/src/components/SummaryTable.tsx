a9ec3b03bd0a71354fbb63252c1c41c2345704a4
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2023-01-18 21:48:54 -0300

    Check habits

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -4,8 +4,9 @@ import { HabitDay } from "./HabitDay";
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const summaryDates = generateDatesFromYearBeginning();
 
+const minimumSummaryDateSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDateSize - summaryDates.length;
 export function SummaryTable() {
-  console.log(summaryDates);
   return (
     <div
       className={`
@@ -33,10 +34,24 @@ export function SummaryTable() {
 
       <div
         className={`
-    		grid grid-col-7 grid-flow-col gap-3
+    		grid grid-rows-7 grid-flow-col gap-3
   			`}>
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className={`
+								w-10 h-10
+								bg-zinc-900
+								border-2 border-zinc-800 rounded-lg
+								opacity-40 cursor-not-allowed
+								`}></div>
+            );
+          })}
       </div>
     </div>
   );
