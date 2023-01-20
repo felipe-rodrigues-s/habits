3550acd84a5b43ab37ee86470fc894c19519dd3a
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2023-01-19 22:12:20 -0300

    ProgressBar

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import * as Progress from "@radix-ui/react-progress";
+
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  const progressStyle = {
+    width: `${props.progress}%`,
+  };
+  return (
+    <Progress.Root
+      className="h-3 rounded-xl bg-zinc-700 w-full m-4"
+      value={100}>
+      <Progress.Indicator
+        className="h-3 rounded-xl bg-violet-600"
+        style={progressStyle}
+      />
+    </Progress.Root>
+  );
+}
